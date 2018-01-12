3ae866926279bbfe189e792c8b855f4981a3a856
truittll <30597279+truittll@users.noreply.github.com>
2018-01-12 10:05:59 -0500

    Update genotoxicity.R

--- genotoxicity.R
+++ genotoxicity.R
@@ -115,8 +115,6 @@ dev.off()
 ###############################################################################################################
 
 x=read.delim("zj48_Vector_contribution.txt")
-head(x)
-setwd("~/Desktop/idalia/FINAL")
 
 lib7=x$LIB7.5[-1]
 lib7=lib7[1:11]
@@ -182,8 +180,7 @@ temp3
 dev.off()
 
 x=read.delim("zj41_Vector_contribution.txt")
-head(x)
-setwd("~/Desktop/idalia/FINAL")
+
 lib7=x$LIB7.5[-1]
 lib7=lib7[1:13]
 lib7
@@ -259,6 +256,89 @@ dev.off()
 ###Unique Barcode Number#######################################################################################
 ###############################################################################################################
 
+grans48=c("pp_50trim_zj48_1_month_Grans_index15_IY12.fastq","pp_50trim_zj48_2_month_Grans_index16_IY12.fastq",
+          "pp_50trim_zj48_3_month_Grans_index17_IY12.fastq","pp_50trim_zj48_4_month_Grans_442p8ng__index18_IY12.fastq",
+          "pp_50trim_ZJ48_5m_Grans_IY_R15_IY24.fastq","pp_50trim_zj48_6_month_Grans_index20_IY12.fastq",
+          "pp_50trim_ZJ48_7m_Grans_IY_R17_IY24.fastq","pp_50trim_ZJ48_11m_Grans_IY_R22_IY24.fastq",
+          "pp_50trim_ZJ48_12m_Grans_IY_R24_IY24.fastq","pp_50trim_ZJ48_16m_Grans_2_IY28_R20.fastq",
+          "ZJ48_Grans_24m_sampled_LT2821_LT3_i524_S19_L001_R1_001.fastq")
+
+B48=c("pp_50trim_zj48_1_month_B_cells_index21_IY12.fastq","pp_50trim_zj48_2_month_B_cells_index22_IY12.fastq",
+      "pp_50trim_zj48_3_month_B_cells_index23_IY12.fastq", "pp_50trim_zj48_4_month_B_cells_index24_IY12.fastq",
+      "pp_50trim_zj48_5_month_B_cells_index25_IY12.fastq","pp_50trim_zj48_6_month_B_cells_index26_IY12.fastq",
+      "pp_50trim_ZJ48_7m_B_cells__IY_R21_IY23.fastq","pp_50trim_ZJ48_9m_B_cells__IY_R25_IY23.fastq",
+      "pp_50trim_ZJ48_12m_B_cells__IY_R27_IY23.fastq","ZJ48_Grans_24m_sampled_LT2821_LT3_i524_S19_L001_R1_001.fastq")
+
+t48=c("pp_50trim_zj48_1_month_T_cells_index15_IY13.fastq","pp_50trim_zj48_2_month_T_cells_index_16_IY13.fastq",
+      "pp_50trim_zj48_3_month_T_cells_index17_IY13.fastq","pp_50trim_zj48_4_month_T_cells_index18_IY13.fastq",
+      "pp_50trim_zj48_5_month_T_cells_index19_IY13.fastq","pp_50trim_zj48_6_month_T_cells_index20_IY13.fastq",
+      "pp_50trim_ZJ48_7m_T_cells__IY_R14_IY23.fastq","pp_50trim_ZJ48_9m_T_cells__IY_R15_IY23.fastq",
+      "pp_50trim_ZJ48_12m_T_cells__IY_R17_IY23.fastq","ZJ48_T_24m_sampled_LT2821_LT3_i528_S22_L001_R1_001.fastq")
+
+
+
+zj48_time_points_g = c(1,2,3,4,5,6,7,11,12,16,24)
+zj48_time_points_t= c(1,2,3,4,5,6,7,9,12,24)
+zj48_time_points_b= c(1,2,3,4,5,6,7,9,12,24)
+
+
+grans41=c("zj41_1m_Gr_pp_50trim_IY21_R15.fastq","zj41_2m_Gr_293ng_pp_50trim_IY21_R16.fastq","zj41_3m_Gr_pp_50trim_IY21_R17.fastq",
+          "zj41_4m_Gr_pp_50trim_IY21_R18.fastq","pp_50trim_ZJ41_5m_Grans_Ann__IY25_R14.fastq","zj41_6m_Gr_pp_50trim_IY21_R20.fastq",
+          "zj41_9m_Gr_pp_50trim_IY21_R21.fastq","zj41_9m_Gr_pp_50trim_IY21_R21.fastq","pp_50trim_ZJ41_13m_Grans_1___IY27_R19.fastq",
+          "pp_50trim_ZJ41_16m_Grans__IY25_R22.fastq","pp_50trim_ZJ41_18m_Grans__IY25_R25.fastq","pp_50trim_ZJ41_21m_Grans__IY25_R26.fastq",
+          "ZJ41_Grans_29m_sampled_LT2821_LT3_i516_S13_L001_R1_001.fastq")
+
+B41=c("zj41_1m_B_pp_50trim_IY15_R23.fastq",
+      "zj41_2m_B_pp_50trim_IY15_R24.fastq",
+      "zj41_3m_B_pp_50trim_IY15_R25.fastq",
+      "zj41_4m_B_pp_50trim_IY15_R26.fastq",
+      "zj41_5m_B_pp_50trim_IY15_R27.fastq",
+      "zj41_6m_B_pp_50trim_IY15_R28.fastq",
+      "zj41_9m_B_rep_pp_50trim_IY15_R34.fastq",
+      "zj41_11m_B_pp_50trim_IY15_R35.fastq",
+      "pp_50trim_ZJ41_12m_B_IY26_R27.fastq",
+      "pp_50trim_ZJ41_18m_B_IY26_R28.fastq",
+      "ZJ41_B_29m_sampled_LT2821_LT3_i518_S14_L001_R1_001.fastq")
+
+
+t41=c("zj41_m1_Tcells_IY09R28.fastq","zj41_m2_Tcells_IY07R28.fastq","zj41_3m_T_rep_pp_50trim_IY22_R25.fastq",
+      "zj41_4m_T_rep_pp_50trim_IY22_R26.fastq","zj41_5m_T_rep_pp_50trim_IY22_R27.fastq","zj41_6m_T_pp_50trim_IY22_R20.fastq",
+      "pp_50trim_ZJ41_7m_T_IY26_R24.fastq","zj41_9m_T_rep_pp_50trim_IY22_R33.fastq","zj41_11m_T_rep_pp_50trim_IY22_R34.fastq",
+      "pp_50trim_ZJ41_18m_T_IY26_R25.fastq","ZJ41_T_29m_sampled_LT2821_LT3_i521_S16_L001_R1_001.fastq")
+
+
+zj41_time_points_g = c(1,2,3,4,5,6,9,12,13,16,18,21,29)
+zj41_time_points_t= c(1,2,3,4,5,6,7,9,11,18,29)
+zj41_time_points_b= c(1,2,3,4,5,6,9,11,12,18,29)
+
+to_plot=list(zj48_lib7[,grans48],zj48_lib8[,grans48],zj48_lib11[,grans48])
+colors=c("deeppink", "darkgrey","deepskyblue")
+zj48_cumulative_grans=plot.cumulative.lib(to_plot,zj48_time_points_g,c("MSCV","EF1a","SFFV"),colors=c("deeppink", "gray","deepskyblue"))
+zj48_unique_grans=plot.unique.lib(to_plot,zj48_time_points_g,c("MSCV","EF1a","SFFV"),colors)
+
+
+to_plot=list(zj48_lib7[,t48],zj48_lib8[,t48],zj48_lib11[,t48])
+zj48_cumulative_t=plot.cumulative.lib(to_plot,zj48_time_points_t,c("MSCV","EF1a","SFFV"),colors)
+
+zj48_unique_t=plot.unique.lib(to_plot,zj48_time_points_t,c("MSCV","EF1a","SFFV"),colors)
+
+to_plot=list(zj48_lib7[,B48],zj48_lib8[,B48],zj48_lib11[,B48])
+zj48_cumulative_b=plot.cumulative.lib(to_plot,zj48_time_points_b,c("MSCV","EF1a","SFFV"),colors)
+zj48_unique_b=plot.unique.lib(to_plot,zj48_time_points_b,c("MSCV","EF1a","SFFV"),colors)
+
+to_plot=list(zj41_lib7[,grans41],zj41_lib8[,grans41],zj41_lib11[,grans41])
+zj41_cumulative_grans=plot.cumulative.lib(to_plot,zj41_time_points_g,c("MSCV","EF1a","SFFV"),colors)
+zj41_unique_grans=plot.unique.lib(to_plot,zj41_time_points_g,c("MSCV","EF1a","SFFV"),colors)
+
+to_plot=list(zj41_lib7[,t41],zj41_lib8[,t41],zj41_lib11[,t41])
+zj41_cumulative_t=plot.cumulative.lib(to_plot,zj41_time_points_t,c("MSCV","EF1a","SFFV"),colors)
+zj41_unique_t=plot.unique.lib(to_plot,zj41_time_points_t,c("MSCV","EF1a","SFFV"),colors)
+
+to_plot=list(zj41_lib7[,B41],zj41_lib8[,B41],zj41_lib11[,B41])
+zj41_cumulative_b=plot.cumulative.lib(to_plot,zj41_time_points_b,c("MSCV","EF1a","SFFV"),colors)
+zj41_unique_b=plot.unique.lib(to_plot,zj41_time_points_b,c("MSCV","EF1a","SFFV"),colors)
+
+
 choose=c(1,2,3,4,5,6,9,18,29)
 temp1=NULL;temp2=NULL;temp3=NULL
 for(i in 1:length(choose)){
@@ -266,7 +346,7 @@ for(i in 1:length(choose)){
   temp2=rbind(temp2,subset(zj41_unique_grans,Month==choose[i]))
   temp3=rbind(temp3,subset(zj41_unique_t,Month==choose[i]))
 }
-temp1
+
 C=temp1$C+temp2$C+temp3$C
 U=temp1$U+temp2$U+temp3$U
 
@@ -300,7 +380,7 @@ for(i in 1:length(choose)){
   temp2=rbind(temp2,subset(zj48_unique_grans,Month==choose[i]))
   temp3=rbind(temp3,subset(zj48_unique_t,Month==choose[i]))
 }
-temp1
+
 C=temp1$C+temp2$C+temp3$C
 U=temp1$U+temp2$U+temp3$U
 
@@ -338,7 +418,7 @@ for(i in 1:length(choose)){
   temp2=rbind(temp2,subset(zj41_unique_grans,Month==choose[i]))
   temp3=rbind(temp3,subset(zj41_unique_t,Month==choose[i]))
 }
-temp1
+
 C=temp1$C+temp2$C+temp3$C
 U=temp1$U+temp2$U+temp3$U
 
@@ -372,7 +452,7 @@ for(i in 1:length(choose)){
   temp2=rbind(temp2,subset(zj48_unique_grans,Month==choose[i]))
   temp3=rbind(temp3,subset(zj48_unique_t,Month==choose[i]))
 }
-temp1
+
 C=temp1$C+temp2$C+temp3$C
 U=temp1$U+temp2$U+temp3$U
 
@@ -420,8 +500,7 @@ dev.off()
 
 plot_data8=plot_data
 plot_data1=plot_data
-plot_data8
-plot_data1
+
 
 save(plot_data8,plot_data1,file="cumulative_data.Rdata")
 ###############################################################################################################
@@ -429,15 +508,99 @@ save(plot_data8,plot_data1,file="cumulative_data.Rdata")
 ###Simpson Index###############################################################################################
 ###############################################################################################################
 
+grans48=c("pp_50trim_zj48_1_month_Grans_index15_IY12.fastq","pp_50trim_zj48_2_month_Grans_index16_IY12.fastq",
+          "pp_50trim_zj48_3_month_Grans_index17_IY12.fastq","pp_50trim_zj48_4_month_Grans_442p8ng__index18_IY12.fastq",
+          "pp_50trim_ZJ48_5m_Grans_IY_R15_IY24.fastq","pp_50trim_zj48_6_month_Grans_index20_IY12.fastq",
+          "pp_50trim_ZJ48_7m_Grans_IY_R17_IY24.fastq",
+          "pp_50trim_ZJ48_12m_Grans_IY_R24_IY24.fastq",
+          "ZJ48_Grans_24m_sampled_LT2821_LT3_i524_S19_L001_R1_001.fastq")
+
+B48=c("pp_50trim_zj48_1_month_B_cells_index21_IY12.fastq","pp_50trim_zj48_2_month_B_cells_index22_IY12.fastq",
+      "pp_50trim_zj48_3_month_B_cells_index23_IY12.fastq", "pp_50trim_zj48_4_month_B_cells_index24_IY12.fastq",
+      "pp_50trim_zj48_5_month_B_cells_index25_IY12.fastq","pp_50trim_zj48_6_month_B_cells_index26_IY12.fastq",
+      "pp_50trim_ZJ48_7m_B_cells__IY_R21_IY23.fastq",
+      "pp_50trim_ZJ48_12m_B_cells__IY_R27_IY23.fastq","ZJ48_Grans_24m_sampled_LT2821_LT3_i524_S19_L001_R1_001.fastq")
+
+t48=c("pp_50trim_zj48_1_month_T_cells_index15_IY13.fastq","pp_50trim_zj48_2_month_T_cells_index_16_IY13.fastq",
+      "pp_50trim_zj48_3_month_T_cells_index17_IY13.fastq","pp_50trim_zj48_4_month_T_cells_index18_IY13.fastq",
+      "pp_50trim_zj48_5_month_T_cells_index19_IY13.fastq","pp_50trim_zj48_6_month_T_cells_index20_IY13.fastq",
+      "pp_50trim_ZJ48_7m_T_cells__IY_R14_IY23.fastq",
+      "pp_50trim_ZJ48_12m_T_cells__IY_R17_IY23.fastq","ZJ48_T_24m_sampled_LT2821_LT3_i528_S22_L001_R1_001.fastq")
+
+tempg=zj48_lib7[,grans48];tempb=zj48_lib7[,B48];tempt=zj48_lib7[,t48]
+temp=c("comb 1m","comb 2m","comb 3m","comb 4m","comb 5m","comb 6m","comb 7m","comb 12m","comb 24m")
+colnames(tempg)=temp
+colnames(tempb)=temp
+colnames(tempt)=temp
+comb_zj48_lib7=rbind(tempg,tempb,tempt)
+
+tempg=zj48_lib8[,grans48];tempb=zj48_lib8[,B48];tempt=zj48_lib8[,t48]
+colnames(tempg)=temp
+colnames(tempb)=temp
+colnames(tempt)=temp
+comb_zj48_lib8=rbind(tempg,tempb,tempt)
+
+tempg=zj48_lib11[,grans48];tempb=zj48_lib11[,B48];tempt=zj48_lib11[,t48]
+colnames(tempg)=temp
+colnames(tempb)=temp
+colnames(tempt)=temp
+comb_zj48_lib11=rbind(tempg,tempb,tempt)
+
+grans41=c("zj41_1m_Gr_pp_50trim_IY21_R15.fastq","zj41_2m_Gr_293ng_pp_50trim_IY21_R16.fastq","zj41_3m_Gr_pp_50trim_IY21_R17.fastq",
+          "zj41_4m_Gr_pp_50trim_IY21_R18.fastq","pp_50trim_ZJ41_5m_Grans_Ann__IY25_R14.fastq","zj41_6m_Gr_pp_50trim_IY21_R20.fastq",
+          "zj41_9m_Gr_pp_50trim_IY21_R21.fastq",
+          "pp_50trim_ZJ41_18m_Grans__IY25_R25.fastq",
+          "ZJ41_Grans_29m_sampled_LT2821_LT3_i516_S13_L001_R1_001.fastq")
+
+B41=c("zj41_1m_B_pp_50trim_IY15_R23.fastq",
+      "zj41_2m_B_pp_50trim_IY15_R24.fastq",
+      "zj41_3m_B_pp_50trim_IY15_R25.fastq",
+      "zj41_4m_B_pp_50trim_IY15_R26.fastq",
+      "zj41_5m_B_pp_50trim_IY15_R27.fastq",
+      "zj41_6m_B_pp_50trim_IY15_R28.fastq",
+      "zj41_9m_B_rep_pp_50trim_IY15_R34.fastq",
+      "pp_50trim_ZJ41_18m_B_IY26_R28.fastq",
+      "ZJ41_B_29m_sampled_LT2821_LT3_i518_S14_L001_R1_001.fastq")
+
+
+t41=c("zj41_m1_Tcells_IY09R28.fastq","zj41_m2_Tcells_IY07R28.fastq","zj41_3m_T_rep_pp_50trim_IY22_R25.fastq",
+      "zj41_4m_T_rep_pp_50trim_IY22_R26.fastq","zj41_5m_T_rep_pp_50trim_IY22_R27.fastq","zj41_6m_T_pp_50trim_IY22_R20.fastq","zj41_9m_T_rep_pp_50trim_IY22_R33.fastq",
+      "pp_50trim_ZJ41_18m_T_IY26_R25.fastq","ZJ41_T_29m_sampled_LT2821_LT3_i521_S16_L001_R1_001.fastq")
+
+zj41_time_points = c(1,2,3,4,5,6,9,18,29)
+temp=c("comb 1m","comb 2m","comb 3m","comb 4m","comb 5m","comb 6m","comb 9m","comb 18m","comb 29m")
+tempg=zj41_lib7[,grans41];tempb=zj41_lib7[,B41];tempt=zj41_lib7[,t41]
+colnames(tempg)=temp
+colnames(tempb)=temp
+colnames(tempt)=temp
+comb_zj41_lib7=rbind(tempg,tempb,tempt)
+
+temp=c("comb 1m","comb 2m","comb 3m","comb 4m","comb 5m","comb 6m","comb 9m","comb 18m","comb 29m")
+tempg=zj41_lib8[,grans41];tempb=zj41_lib8[,B41];tempt=zj41_lib8[,t41]
+colnames(tempg)=temp
+colnames(tempb)=temp
+colnames(tempt)=temp
+comb_zj41_lib8=rbind(tempg,tempb,tempt)
+
+temp=c("comb 1m","comb 2m","comb 3m","comb 4m","comb 5m","comb 6m","comb 9m","comb 18m","comb 29m")
+tempg=zj41_lib11[,grans41];tempb=zj41_lib11[,B41];tempt=zj41_lib11[,t41]
+colnames(tempg)=temp
+colnames(tempb)=temp
+colnames(tempt)=temp
+comb_zj41_lib11=rbind(tempg,tempb,tempt)
+
+zj48_time_points = c(1,2,3,4,5,6,7,12,24)
+zj41_time_points = c(1,2,3,4,5,6,9,18,29)
+
 colors=c("deeppink", "darkgrey","deepskyblue")
 temp=SI_lib(list(comb_zj48_lib7,comb_zj48_lib8,comb_zj48_lib11),
             c("ZJ48 Lib MSCV","ZJ48 EF1a","ZJ41 SFFV"), 
             list(zj48_time_points,zj48_time_points,zj48_time_points),colors)
 
 combined_grans=temp+
-  geom_abline(linetype="dashed",slope=0,intercept=zj48_cd34_lib7, show.legend = TRUE,color=colors[1],size=2)+
-  geom_abline(linetype="dashed",slope=0,intercept=zj48_cd34_lib8, show.legend = TRUE,color=colors[2],size=2)+
-  geom_abline(linetype="dashed",slope=0,intercept=zj48_cd34_lib11, show.legend = TRUE,color=colors[3],size=2)+
+  #geom_abline(linetype="dashed",slope=0,intercept=zj48_cd34_lib7, show.legend = TRUE,color=colors[1],size=2)+
+  #geom_abline(linetype="dashed",slope=0,intercept=zj48_cd34_lib8, show.legend = TRUE,color=colors[2],size=2)+
+  #geom_abline(linetype="dashed",slope=0,intercept=zj48_cd34_lib11, show.legend = TRUE,color=colors[3],size=2)+
   ggtitle("ZJ48")+theme(title=element_text(size=50))
 
 pdf(file = "SI_zj48.pdf",width=30,height=20)
@@ -448,9 +611,9 @@ temp=SI_lib(list(zj41_lib7,zj41_lib8,zj41_lib11),
             c("ZJ48 Lib MSCV","ZJ48 EF1a","ZJ41 SFFV"), 
             list(zj41_time_points,zj41_time_points,zj41_time_points),colors)
 combined_b=temp+
-  geom_abline(linetype="dashed",slope=0,intercept=zj41_cd34_lib7, show.legend = TRUE,color=colors[1],size=2)+
-  geom_abline(linetype="dashed",slope=0,intercept=zj41_cd34_lib8, show.legend = TRUE,color=colors[2],size=2)+
-  geom_abline(linetype="dashed",slope=0,intercept=zj41_cd34_lib11, show.legend = TRUE,color=colors[3],size=2)+
+  #geom_abline(linetype="dashed",slope=0,intercept=zj41_cd34_lib7, show.legend = TRUE,color=colors[1],size=2)+
+  #geom_abline(linetype="dashed",slope=0,intercept=zj41_cd34_lib8, show.legend = TRUE,color=colors[2],size=2)+
+  #geom_abline(linetype="dashed",slope=0,intercept=zj41_cd34_lib11, show.legend = TRUE,color=colors[3],size=2)+
   ggtitle("ZJ41")+theme(title=element_text(size=50))
 
 pdf(file = "SI_zj41.pdf",width=30,height=20)
@@ -461,9 +624,9 @@ temp=SI_lib(list(zj48_lib7,zj48_lib8,zj41_lib11),
             c("ZJ48 Lib MSCV","ZJ48 EF1a","ZJ41 SFFV"), 
             list(zj48_time_points,zj48_time_points,zj41_time_points),colors)
 combined_bb=temp+
-  geom_abline(linetype="dashed",slope=0,intercept=zj48_cd34_lib7, show.legend = TRUE,color=colors[1],size=2)+
-  geom_abline(linetype="dashed",slope=0,intercept=zj48_cd34_lib8, show.legend = TRUE,color=colors[2],size=2)+
-  geom_abline(linetype="dashed",slope=0,intercept=zj41_cd34_lib11, show.legend = TRUE,color=colors[3],size=2)+
+  #geom_abline(linetype="dashed",slope=0,intercept=zj48_cd34_lib7, show.legend = TRUE,color=colors[1],size=2)+
+  #geom_abline(linetype="dashed",slope=0,intercept=zj48_cd34_lib8, show.legend = TRUE,color=colors[2],size=2)+
+  #geom_abline(linetype="dashed",slope=0,intercept=zj41_cd34_lib11, show.legend = TRUE,color=colors[3],size=2)+
   ggtitle("ZJ41")+theme(title=element_text(size=50))
 
 pdf(file = "final_simpson_high.pdf",width=30,height=20)
@@ -481,25 +644,21 @@ dev.off()
 
 colors=c("Greys","RdPu","Blues")
 
-zj48_grans=combined_stacked(list(zj48_lib7,zj48_lib8,zj48_lib11),grans8,zj48_time_points_g,20,plot_title=NULL,colors,n=c(9,9,9),"zj48_grans")+ggtitle("Granulocytes")+ylab("ZJ48 \n Percent Contribution \n of Barcodes")+theme(plot.title = element_text(size=50))
-zj48_t=combined_stacked(list(zj48_lib7,zj48_lib8,zj48_lib11),t8,zj48_time_points_t,20,plot_title=NULL,colors,n=c(9,9,9),"zj48_t")+ggtitle("T Cells")+theme(plot.title = element_text(size=50))
-zj48_b=combined_stacked(list(zj48_lib7,zj48_lib8,zj48_lib11),B8,zj48_time_points_b,20,plot_title=NULL,colors,n=c(9,9,9),"zj48_b")+ggtitle("B Cells")+theme(plot.title = element_text(size=50))
+zj48_grans=combined_stacked(list(zj48_lib7,zj48_lib8,zj48_lib11),grans48,zj48_time_points,20,plot_title=NULL,colors,n=c(9,9,9),"zj48_grans")+ggtitle("Granulocytes")+ylab("ZJ48 \n Percent Contribution \n of Barcodes")+theme(plot.title = element_text(size=50))
+zj48_t=combined_stacked(list(zj48_lib7,zj48_lib8,zj48_lib11),t48,zj48_time_points,20,plot_title=NULL,colors,n=c(9,9,9),"zj48_t")+ggtitle("T Cells")+theme(plot.title = element_text(size=50))
+zj48_b=combined_stacked(list(zj48_lib7,zj48_lib8,zj48_lib11),B48,zj48_time_points,20,plot_title=NULL,colors,n=c(9,9,9),"zj48_b")+ggtitle("B Cells")+theme(plot.title = element_text(size=50))
 
 
-zj41_t=combined_stacked(list(zj41_lib7,zj41_lib8,zj41_lib11),t1,zj41_time_points_t,20,plot_title=NULL,colors,n=c(9,9,9),"zj41_t")
-zj41_b=combined_stacked(list(zj41_lib7,zj41_lib8,zj41_lib11),B1,zj41_time_points_b,20,plot_title=NULL,colors,n=c(9,9,9),"zj41_b")
-zj41_grans
-nrow(zj41_lib7)
-ncol(zj41_lib7)
+zj41_t=combined_stacked(list(zj41_lib7,zj41_lib8,zj41_lib11),t41,zj41_time_points,20,plot_title=NULL,colors,n=c(9,9,9),"zj41_t")
+zj41_b=combined_stacked(list(zj41_lib7,zj41_lib8,zj41_lib11),B41,zj41_time_points,20,plot_title=NULL,colors,n=c(9,9,9),"zj41_b")
+
 temp=matrix(nrow=1085,ncol=35,data=runif(1085*35))
 zj41_lib7=zj41_lib7+temp
 
-nrow(zj41_lib8)
-ncol(zj41_lib8)
 temp=matrix(nrow=1085,ncol=35,data=runif(1085*35))
 zj41_lib8=zj41_lib8+temp
 
-zj41_grans=combined_stacked(list(zj41_lib7,zj41_lib8,zj41_lib11),grans1,zj41_time_points_g,20,plot_title=NULL,colors,n=c(9,9,9),"zj41_grans")+ylab("ZJ41 \n Percent Contribution \n of Barcodes")
+zj41_grans=combined_stacked(list(zj41_lib7,zj41_lib8,zj41_lib11),grans41,zj41_time_points,20,plot_title=NULL,colors,n=c(9,9,9),"zj41_grans")+ylab("ZJ41 \n Percent Contribution \n of Barcodes")
 
 pdf(file = "final_stacked.pdf",width=30,height=20)
 grid.arrange(zj41_grans,zj41_t,zj41_b,zj48_grans,zj48_t,zj48_b,ncol=3)
@@ -523,9 +682,9 @@ dev.off()
 ###Monte Carlo#################################################################################################
 ###############################################################################################################
 
-create_divplot.k("idalia_MC1.txt","","lib7_K","MC")
-create_divplot.k("idalia_MC2.txt","","lib8_K","MC")
-create_divplot.k("idalia_MC3.txt","","lib11_K","MC")
+create_divplot.k("MC1.txt","","lib7_K","MC")
+create_divplot.k("MC2.txt","","lib8_K","MC")
+create_divplot.k("MC3.txt","","lib11_K","MC")
 
 ###############################################################################################################
 ###FIGURE S2###################################################################################################
